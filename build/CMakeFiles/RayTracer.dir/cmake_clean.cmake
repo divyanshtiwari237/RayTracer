a3ccffc7149790fa580f1c25f37bf4895727a296
Divyansh Tiwari <200020049@iitb.ac.in>
2021-10-14 19:56:58 +0530

    Restructed the files and added a simple CMakeLists.txt file

---
+++ build/CMakeFiles/RayTracer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracer.dir/main.cpp.obj"
+  "CMakeFiles/RayTracer.dir/main.cpp.obj.d"
+  "RayTracer.exe"
+  "RayTracer.exe.manifest"
+  "RayTracer.pdb"
+  "libRayTracer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
