ed1eeafc6690ea723e161e5dbed33e1c4eee8e3f
Divyansh Tiwari <200020049@iitb.ac.in>
2022-03-19 03:09:10 +0530

    random changes

--- main/main.cpp
+++ main/main.cpp
@@ -34,7 +34,7 @@ int main()
     std::ofstream obj("../../Images/metal.ppm");
     //image
     const auto aspect_ratio =16.0/9.0;
-    const int image_width = 400;
+    const int image_width = 1920;
     const int image_height = static_cast<int>(image_width/aspect_ratio);
     int samplesperPixel =100;
     const int maxDepth = 50;
